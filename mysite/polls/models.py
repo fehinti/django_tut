e25553fca15c3d733d3c4acad5add60b3677e675
fehinti <victorolufaj@gmail.com>
2021-03-11 11:22:41 +0100

    Part 5 - Creating first django app

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -11,12 +11,13 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    Choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.Choice_text
+        return self.choice_text
